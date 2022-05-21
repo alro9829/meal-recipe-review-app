5e046ed847d715963dd958ddca2c7b75c51ff897
alro9829 <70238044+alro9829@users.noreply.github.com>
2022-05-21 12:45:38 -0600

    adding meal website

---
+++ database/db_script.sql
@@ -0,0 +1,7 @@
+/* Database d2k6qlcsrfghg1 created on Heroku */
+
+d2k6qlcsrfghg1=> CREATE TABLE reviews (
+d2k6qlcsrfghg1(> id SERIAL PRIMARY KEY,
+d2k6qlcsrfghg1(> meal_name text,
+d2k6qlcsrfghg1(> review text,
+d2k6qlcsrfghg1(> review_date date);
